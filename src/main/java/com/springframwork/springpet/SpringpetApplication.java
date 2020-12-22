fcd39184cd662adc30506fd8cbd71e6f33ea36f5
Jack Tran <jacktran@lucidmotors.com>
2020-12-22 14:17:11 -0800

    add src

---
+++ src/main/java/com/springframwork/springpet/SpringpetApplication.java
@@ -0,0 +1,13 @@
+package com.springframwork.springpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpetApplication.class, args);
+    }
+
+}
