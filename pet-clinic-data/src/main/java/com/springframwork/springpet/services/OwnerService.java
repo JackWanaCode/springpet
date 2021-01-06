e0a43db79c7b46199fbd53e8ce3d61216f308a95
Jack Tran <jacktran@lucidmotors.com>
2021-01-06 10:37:52 -0800

    Refactor services

--- pet-clinic-data/src/main/java/com/springframwork/springpet/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/springframwork/springpet/services/OwnerService.java
@@ -2,16 +2,9 @@ package com.springframwork.springpet.services;
 
 import com.springframwork.springpet.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/com/springframwork/springpet/services/PetService.java
+++ pet-clinic-data/src/main/java/com/springframwork/springpet/services/PetService.java
@@ -1,15 +1,7 @@
 package com.springframwork.springpet.services;
 
-import com.springframwork.springpet.model.Owner;
 import com.springframwork.springpet.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
